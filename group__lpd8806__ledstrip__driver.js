c0ea8d8fbeafe8a9f6a2b5ea0f0cef3f5c54ecdf
Joe Kroesche <joe@kroesche.org>
2014-07-27 15:24:30 -0500

    Regen docs following addition of LPD8806 driver

---
+++ group__lpd8806__ledstrip__driver.js
@@ -0,0 +1,24 @@
+var group__lpd8806__ledstrip__driver =
+[
+    [ "LPD8806_LEDSTRIP_ALLOW_DMA", "group__lpd8806__ledstrip__driver.html#ga7d10ae9c15200f4a25cda82c3b3b45b8", null ],
+    [ "LPD8806_LEDSTRIP_NUM_INSTANCES", "group__lpd8806__ledstrip__driver.html#gad73b39112ed578d7d5f5a609a489dfa2", null ],
+    [ "LPD8806_LEDSTRIP_FRAMEBUF_SIZE", "group__lpd8806__ledstrip__driver.html#ga4d893e65bd1866d3108962aeea969c8b", null ],
+    [ "LPD8806_LedStrip_Handle_t", "group__lpd8806__ledstrip__driver.html#ga6918c0b45c319c8a5f3c45326877e6cf", null ],
+    [ "LPD8806_LedStrip_Channel_t", "group__lpd8806__ledstrip__driver.html#gafec4c9a708a43cd1562d7d2716844d5f", [
+      [ "CHAN_RED", "group__lpd8806__ledstrip__driver.html#ggafec4c9a708a43cd1562d7d2716844d5fa1fcfb27bf044547abacc99185b5ee17d", null ],
+      [ "CHAN_GREEN", "group__lpd8806__ledstrip__driver.html#ggafec4c9a708a43cd1562d7d2716844d5fa038c793c694357b3c116e5385d4582ed", null ],
+      [ "CHAN_BLUE", "group__lpd8806__ledstrip__driver.html#ggafec4c9a708a43cd1562d7d2716844d5fa351320859843f1af295c3fd4367cb533", null ]
+    ] ],
+    [ "LPD8806_LedStrip_SetPixelValue", "group__lpd8806__ledstrip__driver.html#ga32c0535b4769b8756ae80ad307b34ead", null ],
+    [ "LPD8806_LedStrip_GetPixelValue", "group__lpd8806__ledstrip__driver.html#ga73b1474c9738e79c376be6a91bd4783c", null ],
+    [ "LPD8806_LedStrip_SetPixelRgb", "group__lpd8806__ledstrip__driver.html#ga6c3333c8001723f92c35f23aaafea519", null ],
+    [ "LPD8806_LedStrip_GetPixelRgb", "group__lpd8806__ledstrip__driver.html#ga009e33f9070e1942b0db7402e6854c63", null ],
+    [ "LPD8806_LedStrip_SetPixelChannel", "group__lpd8806__ledstrip__driver.html#ga13bb90d63c7b0346c4a3e2a21ec790e5", null ],
+    [ "LPD8806_LedStrip_GetPixelChannel", "group__lpd8806__ledstrip__driver.html#ga4d1b90d05e74027e8cee753a336b722d", null ],
+    [ "LPD8806_LedStrip_Update", "group__lpd8806__ledstrip__driver.html#ga96edb00eda7a42e2c3ee4489289d21f9", null ],
+    [ "LPD8806_LedStrip_NumPixels", "group__lpd8806__ledstrip__driver.html#ga8427508e09ccaab02eda02980ba88e7d", null ],
+    [ "LPD8806_LedStrip_Init", "group__lpd8806__ledstrip__driver.html#gabad153a5e74ddb95bab883552d163544", null ],
+    [ "LPD8806_LedStrip_SetFrameBuffer", "group__lpd8806__ledstrip__driver.html#ga0e33e4da250682bfdc0f2602bb72b835", null ],
+    [ "LPD8806_LedStrip_Clear", "group__lpd8806__ledstrip__driver.html#ga3ce3fc57c7d5c7c8ded09cc587ba714f", null ],
+    [ "LPD8806_LedStrip_EnableDma", "group__lpd8806__ledstrip__driver.html#gaaf62fec53810f6f85f8bc82bb1f46a35", null ]
+];
--- modules.js
+++ modules.js
@@ -1,4 +1,5 @@
 var modules =
 [
+    [ "Driver for LPD8806 RGB LED Strips", "group__lpd8806__ledstrip__driver.html", "group__lpd8806__ledstrip__driver" ],
     [ "Servo Driver for Wide Timers", "group__servo__wt__driver.html", "group__servo__wt__driver" ]
 ];
